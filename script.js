0b69233a96531e60ab48f836c5d1da0b64e2ea59
NiTzuA <11v35faustin@gmail.com>
2025-07-02 09:05:48 +0800

    Created operate function.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let numOne;
+let operator;
+let numTwo;
+
 function add(numOne, numTwo) {
     return numOne + numTwo;
 }
@@ -12,4 +16,17 @@ function multiply(numOne, numTwo) {
 
 function divide(numOne, numTwo) {
     return numTwo == 0 ? "Cannot divide by zero!" : numOne / numTwo;
+}
+
+function operate(numOne, numTwo, operator) {
+    numOne = Number(numOne);
+    numTwo = Number(numTwo);
+
+    switch (operator) {
+        case '+': return add(numOne, numTwo);
+        case '-': return subtract(numOne, numTwo);
+        case '*': return multiply(numOne, numTwo);
+        case '/': return divide(numOne, numTwo);
+        default: return "Invalid Operator"
+    }
 }
