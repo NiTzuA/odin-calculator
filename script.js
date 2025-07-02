f6d1a5c312a969a93a9e0cb6d5cf49d917f752dd
NiTzuA <11v35faustin@gmail.com>
2025-07-02 10:13:07 +0800

    Wrote logic for handling button inputs.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-let numOne;
-let operator;
-let numTwo;
+let numOne = "";
+let operator = "";
+let numTwo = "";
+let isNumTwo = false;
+const operators = ['+', '-', '*', '/'];
 
 function add(numOne, numTwo) {
     return numOne + numTwo;
@@ -30,3 +32,46 @@ function operate(numOne, numTwo, operator) {
         default: return "Invalid Operator"
     }
 }
+
+const calculatorButtons = document.querySelector('#calculator');
+
+calculatorButtons.addEventListener('click', (e) => {
+    if (e.target.tagName === 'BUTTON') {
+
+        const value = e.target.textContent;
+
+        if (value == 'C') {
+            if (!isNumTwo) {
+                numOne = numOne.slice(0, -1);
+            } else {
+                numTwo = numTwo.slice(0, -1);
+            }
+        }
+        else if (value == '=') {
+            if (numTwo === "") {
+                alert("Input the second number!");
+            } else {
+                result = operate(numOne, numTwo, operator);
+                numOne = "";
+                numTwo = "";
+                operator = "";
+                isNumTwo = false;
+                alert(result);
+            }
+        }
+        else if (operators.includes(value)) {
+            if (numOne === "") {
+                alert("Input a number first!");
+            } else {
+                isNumTwo = true;
+                operator = value;
+            }
+        }
+        else if (!isNumTwo) {
+            numOne += value;
+        } 
+        else if (isNumTwo) {
+            numTwo += value;    
+        }
+    }
+});
